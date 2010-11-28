b4c48b0e7b64028a16ec88e222b7869d28a5adb9
Richard Russon (flatcap) <richard.russon@gmail.com>
2010-11-28 21:40:11 +0000

    more work files

---
+++ jim.php
@@ -0,0 +1,40 @@
+<?php
+
+$input  = '46 p/w(d) "reachy"'	. "\n";
+$input  = '46 p/w(d) "reachy"'	. "\n";
+
+
+function get_colours()
+{
+	$colours = array(
+		array (1,	"Beige",	"beige,beig,bei,be,bg"),
+		array (2,	"Blue",		"blue,blu"),
+		array (3,	"Blue/White",	"b/w"),
+		array (4,	"Features",	"features,feature,featur,featu,feat,fea,fe,ft"),
+		array (5,	"Green",	"green,gree,gren,grn,gn"),
+		array (6,	"Grey",		"grey,gray,gry,gy"),
+		array (7,	"Mushroom",	"mushroom,mushroo,mushro,mushr,mush,mus,mu"),
+		array (8,	"Orange",	"orange,orang,oran,ora,or"),
+		array (9,	"Orange/White",	"o/w"),
+		array (10,	"Pink",		"pink,pin,pi,pk"),
+		array (11,	"Pink/Black",	"p/b"),
+		array (12,	"Purple",	"purple,purpl,purp,pur,pu,pp"),
+		array (13,	"Purple/White",	"p/w"),
+		array (14,	"Red",		"red,re,rd"),
+		array (15,	"Red/White",	"r/w"),
+		array (16,	"Turquoise",	"turquoise,turquois,turquoi,turquo,turqu,turq,tur,tq,tr"),
+		array (17,	"White",	"white,whit,whi,wh,w"),
+		array (18,	"Yellow",	"yellow,yello,yell,yel,ye,yl,y"),
+		array (19,	"Black",	"black,blac,bla,blk,bk"),
+		array (20,	"Brown",	"brown,brow,bro,brn,bn"),
+		array (21,	"Tiger",	"tiger,tige,tig,ti,tg"));
+
+	return $colours;
+}
+
+
+$c = get_colours();
+
+//var_dump ($c);
+
+var_dump ($argv);
---
+++ transfer.php
@@ -0,0 +1,65 @@
+<?php
+
+// Transfer the climb data from the route table to the climbs table
+
+set_include_path ("/home/flatcap/downloads/update/www");
+
+include "db.php";
+
+/* CLIMBS
+ *	id
+ *	climber_id
+ *	route_id
+ *	date_climbed
+ *	success
+ *	downclimb
+ *	nice
+ *	difficulty
+ *	notes
+ *
+ * ROUTE
+ *	id			X
+ *	panel
+ *	colour
+ *	grade
+ *	notes
+ *	setter
+ *	date_set
+ *	date_climbed		X
+ *	success			X
+ *	downclimb		X
+ *	nice			X
+ *	onsight			X
+ *	difficulty		X
+ *	climb_notes		X
+ */
+
+$routes = db_select("route");
+
+foreach ($routes as $id => $row) {
+	// insert into climbs
+	$climber_id	= 1;
+	$route_id	= $row['id'];
+	$date_climbed	= $row['date_climbed'];
+	$success	= $row['success'];
+	$downclimb	= $row['downclimb'];
+	$nice		= $row['nice'];
+	$onsight	= $row['onsight'];
+	$difficulty	= $row['difficulty'];
+	$climb_notes	= $row['climb_notes'];
+
+	if ($date_climbed == "0000-00-00")
+		continue;
+
+	$sql  = "INSERT INTO climbs (climber_id,route_id,date_climbed,success,downclimb,nice,onsight,difficulty,notes) ";
+	$sql .= "VALUES ('{$climber_id}','{$route_id}','{$date_climbed}','{$success}','{$downclimb}','{$nice}','{$onsight}','{$difficulty}','{$climb_notes}')";
+
+	$result = mysql_query($sql);
+	if ($result) {
+		$id = mysql_insert_id();
+	} else {
+		printf ("error for: $sql\n");
+		break;
+	}
+}
+

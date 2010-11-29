6462d67e9a5085a0bcd05668381e10cde2badcc7
Richard Russon (flatcap) <richard.russon@gmail.com>
2010-11-29 00:37:22 +0000

    html/js/php communicate and give out panel/route info

--- www/getclimbs.php
+++ www/getclimbs.php
@@ -1,70 +1,91 @@
 <?php
-// Fill up array with names
-$a[]="Anna";
-$a[]="Brittany";
-$a[]="Cinderella";
-$a[]="Diana";
-$a[]="Eva";
-$a[]="Fiona";
-$a[]="Gunda";
-$a[]="Hege";
-$a[]="Inga";
-$a[]="Johanna";
-$a[]="Kitty";
-$a[]="Linda";
-$a[]="Nina";
-$a[]="Ophelia";
-$a[]="Petunia";
-$a[]="Amanda";
-$a[]="Raquel";
-$a[]="Cindy";
-$a[]="Doris";
-$a[]="Eve";
-$a[]="Evita";
-$a[]="Sunniva";
-$a[]="Tove";
-$a[]="Unni";
-$a[]="Violet";
-$a[]="Liza";
-$a[]="Elizabeth";
-$a[]="Ellen";
-$a[]="Wenche";
-$a[]="Vicky";
 
-//get the q parameter from URL
-$q=$_GET["q"];
+include "db.php";
+include "utils.php";
 
-//lookup all hints from array if length of q>0
-if (strlen($q) > 0)
-  {
-  $hint="";
-  for($i=0; $i<count($a); $i++)
-    {
-    if (strtolower($q)==strtolower(substr($a[$i],0,strlen($q))))
-      {
-      if ($hint=="")
-        {
-        $hint=$a[$i];
-        }
-      else
-        {
-        $hint=$hint." , ".$a[$i];
-        }
-      }
-    }
-  }
+$g_routes  = NULL;
+$g_panels  = NULL;
+$g_colours = NULL;
 
-// Set output to "no suggestion" if no hint were found
-// or to the correct values
-if ($hint == "")
-  {
-  $response="no suggestion";
-  }
-else
-  {
-  $response=$hint;
-  }
 
-//output the response
-echo $response;
+if (array_key_exists ('q', $_GET)) $q = trim ($_GET["q"]); else $q = "";
+if (array_key_exists ('r', $_GET)) $r = trim ($_GET["r"]); else $r = "";
+
+if (!empty ($r)) {
+	echo "$r";
+	return;
+}
+
+if (isset ($argc)) {
+	if ($argc < 2) {
+		printf ("args\n");
+		return;
+	}
+	array_shift ($argv);
+	$q = implode (" ", $argv);
+} else {
+	if (!isset ($_GET)) {
+		echo "bad call: no get";
+		return;
+	}
+		
+	if (!array_key_exists ('q', $_GET)) {
+		echo "bad call: no q";
+		return;
+	}
+
+	$q=trim ($_GET["q"]);
+	if (empty ($q)) {
+		//echo "empty";
+		return;
+	}
+}
+
+$parts = preg_split("/[\s,]+/", $q);
+$panel = array_shift ($parts);
+
+if (!is_numeric ($panel)) {
+	printf ("Not a panel number: '%s'", $panel);
+	return;
+}
+
+$g_routes  = db_select("route");
+$g_colours = db_select("colour");
+$g_panels  = db_select("panel");
+
+$panel_id = NULL;
+foreach ($g_panels as $id => $p) {
+	if ($p['number'] == $panel) {
+		$panel_id = $p['id'];
+		break;
+	}
+}
+
+if ($panel_id === NULL) {
+	printf ("Panel '%d' doesn't exist\n", $panel);
+	return;
+}
+
+$matches = array();
+foreach ($g_routes as $id => $r) {
+	if ($r['panel'] == $panel_id) {
+		$col_id = $r['colour'];
+		$col = $g_colours[$r['colour']]['colour'];
+		$matches[$col] = $r;
+	}
+}
+
+$num_routes = count ($matches);
+if ($num_routes == 0) {
+	printf ("Panel '%d' doesn't have any routes", $panel);
+	return;
+}
+
+$list = array();
+foreach ($matches as $m) {
+	$list[] = $g_colours[$m['colour']]['colour'];
+}
+printf ("%d %s", $panel, implode (", ", $list));
+
+//echo $response;
 

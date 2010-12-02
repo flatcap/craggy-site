2ea71e446841177ee1c56516d158485c5e48d1e1
Richard Russon (flatcap) <richard.russon@gmail.com>
2010-12-02 11:23:17 +0000

    live conf

--- www/conf.php
+++ www/conf.php
@@ -2,8 +2,6 @@
 
 $db_host     = "localhost";
 $db_user     = "craggy";
-$db_pass     = "jilkerm";
+$db_pass     = "Vailae7j";
 $db_database = "craggy";
 
-$google_user = "richard.russon";
-$google_pass = "addlestones";

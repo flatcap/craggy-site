921e220f41478d322cd3e66c7c02b005c8cad433
Richard Russon (flatcap) <richard.russon@gmail.com>
2010-12-02 11:46:07 +0000

    update images, don't talk to google

---
+++ update/image_update.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+PATH="/bin:/usr/bin"
+
+# Change to the working directory
+pushd ${0%/*} > /dev/null
+
+cleanup()
+{
+	rm -f *.svg
+	rm -f *.png
+	rm -f *.dat
+}
+
+convert()
+{
+	[ -n "$1" ] || exit 1
+
+	local BASE=$(basename "$1" .svg)
+	rsvg-convert --background-color white "$1" > "$BASE.png"
+}
+
+
+cleanup
+
+# Generate and install the graphics
+for i in age colour grade; do
+	php $i.php > $i.dat
+	gnuplot $i.plot
+	convert $i.svg
+	install -m 644 $i.png ../www/img
+done
+
+cleanup
+
+popd > /dev/null
+exit 0
+

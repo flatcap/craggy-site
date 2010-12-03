a5ab0a9a3509ca93db8d5995559936190e929c9e
Richard Russon (flatcap) <richard.russon@gmail.com>
2010-12-03 02:42:30 +0000

    serve up and parse xml data

--- examples/serve_xml/serve.js
+++ examples/serve_xml/serve.js
@@ -10,21 +10,50 @@ function route_initialise (entry_id, matches_id)
 	route_entry.focus();
 }
 
+function route_get_node (node, name)
+{
+	try {
+		txt = node.getElementsByTagName(name)[0].firstChild.nodeValue;
+	} catch (er) {
+		txt = "&nbsp;";
+	}
+
+	return "<td>" + txt + "</td>";
+}
+
 function route_callback()
 {
-	if ((xmlhttp.readyState == 4) && (xmlhttp.status == 200)) {
-		var response = xmlhttp.responseText;
+	if ((xmlhttp.readyState != 4) || (xmlhttp.status != 200))
+		return;
 
-		split = response.indexOf (" - ");
-		if (split >= 0) {
-			start = response.substring (0, split);
-			end   = response.substring (split+3);
-			route_entry.value = start;
-			route_matches.innerHTML = end;
-		} else {
-			route_matches.innerHTML = response;
-		}
+	txt =	"<table cellspacing=0 border=1><tr>" +
+		"<th>ID</th>" +
+		"<th>Route ID</th>" +
+		"<th>Date Climbed</th>" +
+		"<th>Success</th>" +
+		"<th>Nice</th>" +
+		"<th>Onsight</th>" +
+		"<th>Difficulty</th>" +
+		"<th>Notes</th>" +
+		"</tr>";
+
+	x=xmlhttp.responseXML.documentElement.getElementsByTagName("climb");
+	for (i = 0; i < x.length; i++) {
+
+		txt = txt + "<tr>";
+		txt = txt + route_get_node (x[i], "id");
+		txt = txt + route_get_node (x[i], "route_id");
+		txt = txt + route_get_node (x[i], "date_climbed");
+		txt = txt + route_get_node (x[i], "success");
+		txt = txt + route_get_node (x[i], "nice");
+		txt = txt + route_get_node (x[i], "onsight");
+		txt = txt + route_get_node (x[i], "difficulty");
+		txt = txt + route_get_node (x[i], "notes");
+		txt = txt + "</tr>";
 	}
+	txt = txt + "</table>";
+
+	route_matches.innerHTML = txt;
 }
 
 function route_onkeyup()
--- examples/serve_xml/serve.php
+++ examples/serve_xml/serve.php
@@ -3,52 +3,47 @@
 set_include_path (".:../../www");
 
 include "db.php";
-include "utils.php";
+#include "utils.php";
 
-if (!isset ($_GET))
-	return;
-	
-if (!array_key_exists ('q', $_GET))
-	return;
+#if (!isset ($_GET))
+#	return;
+#	
+#if (!array_key_exists ('q', $_GET))
+#	return;
+#
+#$q=trim($_GET["q"]);
+#if ($q != "climb")
+#	return;
 
-$q=trim($_GET["q"]);
-if ($q != "climb")
-	return;
+#$g_routes  = db_select("route");
+#$g_colours = db_select("colour");
+#$g_panels  = db_select("panel");
 
-$g_routes  = db_select("route");
-$g_colours = db_select("colour");
-$g_panels  = db_select("panel");
+header('Content-Type: application/xml; charset=ISO-8859-1');
 
 $table   = "climbs";
 $columns = array ("id", "route_id", "date_climbed", "success", "nice", "onsight", "difficulty", "notes");
 $where   = array ("climber_id = 1"); 
 $order   = NULL;
-$climbs = db_select($table, $columns, $where, $order);
 
-$num_climbs = count ($climbs);
-if ($num_climbs == 0)
-	return;
+$climb_list = db_select($table, $columns, $where, $order);
 
 $count = 0;
-$output = "";
-$output = "<table border=1 cellspacing=0><tr>";
-foreach ($columns as $name) {
-	$output .= "<th>{$name}</th>";
-}
-$output .= "</tr>";
+$output = "<?xml version='1.0' encoding='UTF-8' standalone='yes'?>\n";
+$output .= "<climbs>\n";
 
-foreach ($climbs as $c) {
-	$output .= "<tr>";
-	foreach ($c as $name) {
-		if (empty ($name))
-			$name = "&nbsp;";
-		$output .= "<td>{$name}</td>";
+foreach ($climb_list as $climb) {
+	$output .= "\t<climb>\n";
+	foreach ($columns as $name) {
+		$value = $climb[$name];
+		$output .= "\t\t<$name>$value</$name>\n";
 	}
-	$output .= "</tr>";
+	$output .= "\t</climb>\n";
 	$count++;
-	if ($count > 10)
+	if ($count >= 10)
 		break;
 }
 
-printf ("%d climbs", $num_climbs);
+$output .= "</climbs>\n";
+
 echo $output;

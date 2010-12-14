0ce66a6bd339dbf8917e1fe2d8ebe3cf1d412b1e
Richard Russon (flatcap) <richard.russon@gmail.com>
2010-12-14 01:57:26 +0000

    removed old rss code

--- rss/routes.sql
+++
@@ -1,3 +0,0 @@
-select distinct date_set from route order by date_set desc limit 15;
-select panel,colour,grade from v_route where date_set >= "2010-08-06";
-select panel,colour,grade,date_set from v_route where date_set >= "2010-08-06" order by date_set desc, panel, grade_num;

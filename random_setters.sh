71856a889082269e5228d71c111a712da491f47a
Richard Russon (flatcap) <richard.russon@gmail.com>
2010-12-17 19:20:44 +0000

    multiple insert values - now much faster

--- random_setters.sh
+++ random_setters.sh
@@ -2,18 +2,29 @@
 
 BOYS="$HOME/git/tips/boys.txt"
 GIRLS="$HOME/git/tips/girls.txt"
+SURNAMES="$HOME/git/tips/surnames.txt"
 
 # clear tables
+echo
 echo "truncate craggy_climb;"
 echo "truncate craggy_climber;"
 echo "truncate craggy_route;"
 echo "truncate craggy_setter;"
+echo
 
 # 10 random girl setters
-sort --random-sort $GIRLS | sed 's/.*/insert into craggy_setter (initials,name) values ("\0","\0");/;10q'
+echo "insert into craggy_setter (initials, first_name, surname) values "
+paste	<(sort --random-sort girls.txt)											\
+	<(sort --random-sort surnames.txt) | head -n 10 |								\
+	awk '{ printf "\t(\"%s%s\", \"%s\", \"%s\"),\n", tolower(substr($1,1,1)), tolower(substr($2,1,1)), $1, $2 }' |	\
+	sed '$s/,$/;\n/'
 
 # 100 random boy climbers
-sort --random-sort $BOYS | sed 's/.*/insert into craggy_climber (name) values ("\0");/;100q'
+echo "insert into craggy_climber (first_name, surname) values"
+paste	<(sort --random-sort boys.txt)											\
+	<(sort --random-sort surnames.txt) | head -n 100 |								\
+	awk '{ printf "\t(\"%s\", \"%s\"),\n", $1, $2 }' |								\
+	sed '$s/,$/;\n/'
 
 # 200 routes
 #  21 colours
@@ -21,6 +32,7 @@ sort --random-sort $BOYS | sed 's/.*/insert into craggy_climber (name) values ("
 #  21 grades
 #  10 setters
 # 200 max days old
+echo "insert into craggy_route (panel_id, colour_id, grade_id, setter_id, date_set) values"
 for ((i = 0; i < 200; i++)); do
 	PANEL=$((RANDOM%86+1))
 	COLOUR=$((RANDOM%21+1))
@@ -29,14 +41,15 @@ for ((i = 0; i < 200; i++)); do
 	DAYS_AGO=$((RANDOM%200))
 	DATE=$(date -d "$DAYS_AGO days ago" "+%Y-%m-%d")
 
-	echo "insert into craggy_route (panel_id, colour_id, grade_id, setter_id, date_set) values ($PANEL,$COLOUR,$GRADE,$SETTER,'$DATE');"
-done
+	echo -e "\t($PANEL, $COLOUR, $GRADE, $SETTER, '$DATE'),"
+done | sed '$s/,$/;\n/'
 
 # 5000 climbs
 # 100 climber_id
 # 200 route_id
 # 200 days ago date_climbed
 # 4 success_id
+echo "insert into craggy_climb (climber_id, route_id, success_id, date_climbed) values"
 for ((i = 0; i < 5000; i++)); do
 	CLIMBER=$((RANDOM%100+1))
 	ROUTE=$((RANDOM%200+1))
@@ -44,6 +57,6 @@ for ((i = 0; i < 5000; i++)); do
 	DAYS_AGO=$((RANDOM%200))
 	DATE=$(date -d "$DAYS_AGO days ago" "+%Y-%m-%d")
 
-	echo "insert into craggy_climb (climber_id, route_id, success_id, date_climbed) values ($CLIMBER,$ROUTE,$SUCCESS,'$DATE');"
-done
+	echo -e "\t($CLIMBER, $ROUTE, $SUCCESS, '$DATE'),"
+done | sed '$s/,$/;\n/'
 

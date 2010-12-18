9fed445f8eb977132379a4c289f0964421c51b88
Richard Russon (flatcap) <richard.russon@gmail.com>
2010-12-18 23:00:44 +0000

    multiple independent ratings

--- examples/rating/rating.js
+++ examples/rating/rating.js
@@ -1,74 +1,117 @@
-var star_count;
-var span_status;
-var rated;
+var all_id     = new Array();
+var all_text   = new Array();
+var all_status = new Array();
+var all_locked = new Array();
+var all_css    = new Array();
 
-// Array
-//     [ description = "Quite Easy", id = "42" ]
-//     ...
-
-function rating_initialise (div, names, description, status)
+function rating_initialise (div, status, desc, ids, texts, css)
 {
 	var div = document.getElementById (div);
-	star_count = names.length;
+	star_count = ids.length;
 
-	for (var i = 0; i < star_count; i++) {
-		var link = document.createElement('a');
-		link.setAttribute ('title', names[i]);
-		link.setAttribute ('id', '_' + i);
+	var index = all_id.length;
+	div.className = "rating"
+	div.id = index;
+
+	all_id.push     (ids);
+	all_text.push   (texts);
+	all_status.push (document.getElementById (status));
+	all_locked.push (false);
+	all_css.push    (css);
+
+	for (var i = 0; i < ids.length; i++) {
+		var link = document.createElement ('a');
+		link.id          = i;
 		link.onclick     = rating_click;
 		link.onmouseover = rating_mouseover;
 		link.onmouseout  = rating_mouseout;
 		div.appendChild (link); 
 	}
 
-	span_status = document.getElementById (status);
+	rating_status (index, desc);
 }
+
+
+function get_star (index, item)
+{
+	var div = get_div (item);
+	var ch = div.childNodes;
+
+	for (var i = 0; i < ch.length; i++) {
+		if (ch[i] == item)
+			return all_css[index][i];
+	}
+
+	return all_css[index][0];
+}
+
+function get_index (item)
+{
+	return get_div (item).id;
+}
+
+function get_div (item)
+{
+	return item.parentNode;
+}
+
 
 function rating_mouseover()
 {
-	if (!rated)
-		rating_refresh (this);
+	var index = get_index (this);
+	if (!all_locked[index])
+		rating_refresh (index, this);
 }
 
 function rating_mouseout()
 {
-	if (rated)
+	var index = get_index (this);
+	if (all_locked[index])
 		return;
 
-	for (i = 0; i < star_count; i++) {
-		document.getElementById ("_" + i).className = "";
-		rating_status (this.parentNode.title);
+	var div = get_div (this);
+	var star_count = all_id[index].length;
+
+	var children = div.childNodes;
+	var c = get_star (index, this);
+	for (var i = 0; i < children.length; i++) {
+		children[i].className = "";
+		rating_status (index, this.parentNode.title);
 	}
 }
 
-function rating_status (text)
+function rating_status (index, text)
 {
-	if (span_status)
-		span_status.innerHTML = text;
+	if (all_status[index])
+		all_status[index].innerHTML = text;
 }
 
-function rating_refresh (item)
+function rating_refresh (index, item)
 {
-	rating_status (item.title);
-	s = item.id.replace ("_", '');
-	for (i = 0; i < star_count; i++) {
-		if (i <= s) {
-			document.getElementById ("_" + i).className = "on" + s;
-		} else {
-			document.getElementById ("_" + i).className = "";
+	var div = get_div (item);
+	var star_count = all_id[index].length;
+
+	var children = div.childNodes;
+	var c = get_star (index, item);
+	for (var i = 0; i < children.length; i++) {
+		children[i].className = c;
+		if (children[i] == item) {
+			rating_status (index, all_text[index][i]);
+			c = "";
 		}
 	}
 }
 
 function rating_click()
 {
-	if (rated) {
-		rated = 0;
-		rating_status (this.title);
-		rating_refresh (this);
+	var index = get_index (this);
+	if (all_locked[index]) {
+		all_locked[index] = false;
+		rating_status (index, all_text[index][this.id]);
+		rating_refresh (index, this);
 	} else {
-		rating_status (this.title + " - Saved");
-		rated = 1;
+		rating_status (index, all_text[index][this.id] + " - Saved");
+		all_locked[index] = true;
 		//alert ("Your rating was: " + this.title);
 	}
 }

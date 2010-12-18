8a899cf9fe42d8a08faaec69ee7325fd27c2c234
Richard Russon (flatcap) <richard.russon@gmail.com>
2010-12-18 18:02:19 +0000

    click to save then click to release

--- examples/rating/rating.js
+++ examples/rating/rating.js
@@ -1,9 +1,12 @@
 var star_count;
-var holder;	// Is the holding pattern for clicked state
-var preSet;	// value once a selection has been made
+var span_status;
 var rated;
 
-function rating_initialise (div, names)
+// Array
+//     [ description = "Quite Easy", id = "42" ]
+//     ...
+
+function rating_initialise (div, names, description, status)
 {
 	var div = document.getElementById (div);
 	star_count = names.length;
@@ -17,25 +20,14 @@ function rating_initialise (div, names)
 		link.onmouseout  = rating_mouseout;
 		div.appendChild (link); 
 	}
+
+	span_status = document.getElementById (status);
 }
 
 function rating_mouseover()
 {
-	if (rated)
-		return;
-
-	s = this.id.replace ("_", ''); // Get the selected star
-	a = 0;
-	for (i = 0; i < star_count; i++) {
-		if (i <= s) {
-			document.getElementById ("_" + i).className = "on" + s;
-			document.getElementById ("rateStatus").innerHTML = this.title;
-			holder = a + 1;
-			a++;
-		} else {
-			document.getElementById ("_" + i).className = "";
-		}
-	}
+	if (!rated)
+		rating_refresh (this);
 }
 
 function rating_mouseout()
@@ -43,13 +35,27 @@ function rating_mouseout()
 	if (rated)
 		return;
 
-	if (preSet) {
-		rating_mouseover (preSet);
-		document.getElementById ("rateStatus").innerHTML = document.getElementById ("ratingSaved").innerHTML;
-	} else {
-		for (i = 0; i < star_count; i++) {
+	for (i = 0; i < star_count; i++) {
+		document.getElementById ("_" + i).className = "";
+		rating_status (this.parentNode.title);
+	}
+}
+
+function rating_status (text)
+{
+	if (span_status)
+		span_status.innerHTML = text;
+}
+
+function rating_refresh (item)
+{
+	s = item.id.replace ("_", '');
+	for (i = 0; i < star_count; i++) {
+		if (i <= s) {
+			document.getElementById ("_" + i).className = "on" + s;
+			rating_status (item.title);
+		} else {
 			document.getElementById ("_" + i).className = "";
-			document.getElementById ("rateStatus").innerHTML = this.parentNode.title;
 		}
 	}
 }
@@ -57,15 +63,13 @@ function rating_mouseout()
 function rating_click()
 {
 	if (rated) {
-		preSet = null;
 		rated = 0;
-		document.getElementById ("rateStatus").innerHTML = this.title;
-		return;
+		rating_status (this.title);
+		rating_refresh (this);
+	} else {
+		rating_status (this.title + " - Saved");
+		rated = 1;
+		//alert ("Your rating was: " + sel.title);
 	}
-
-	document.getElementById ("rateStatus").innerHTML = this.title + " - Saved";
-	preSet = this;
-	rated = 1;
-	//alert ("Your rating was: " + sel.title);
 }
 

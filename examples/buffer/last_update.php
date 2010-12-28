a1c90dadd8402d7900bd41a41930243103d2d205
Richard Russon (flatcap) <richard.russon@gmail.com>
2010-12-28 13:00:17 +0000

    time a simple database request

---
+++ examples/buffer/last_update.php
@@ -0,0 +1,22 @@
+<?php
+
+set_include_path ("../../libs");
+
+include "db.php";
+
+date_default_timezone_set("UTC");
+
+$db = db_get_database();
+$query = "select value from craggy_data where name = 'last_update'";
+
+$time_start = microtime (true);
+$result = mysql_query($query);
+$time_end   = microtime (true);
+
+$row = mysql_fetch_array($result, MYSQL_ASSOC);
+mysql_free_result($result);
+
+$diff = $time_end - $time_start;
+
+printf ("result = %s, elapsed = %.6f (1/%.0f) seconds\n", $row['value'], $diff, 1/$diff);
+

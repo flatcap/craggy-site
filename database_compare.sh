388e1ecc3ed3f98aadd7721727895135a507b469
Richard Russon (flatcap) <richard.russon@gmail.com>
2010-12-29 01:55:24 +0000

    ignore definer

--- database_compare.sh
+++ database_compare.sh
@@ -6,12 +6,14 @@ OPTS="--skip-extended-insert --add-drop-database --database craggy --skip-dump-d
 
 if [ ! -f craggy_russon.sql ]; then
 	mysqldump $OPTS -h127.0.0.1 -P3307 -ubackup -pphokio10 | sed -e '/^-- Server version/d' \
-									    -e 's/^-- Host: 127.0.0.1/-- Host: russon.org/' \
+								     -e 's/^-- Host: 127.0.0.1/-- Host: russon.org/' \
+								     -e 's/\(DEFINER=`\)root/\1craggy/' \
 								> craggy_russon.sql
 fi
 
-mysqldump $OPTS -h127.0.0.1 -P3306 -ucraggy            | sed -e '/^-- Server version/d' \
-								    -e 's/^-- Host: 127.0.0.1/-- Host: flatcap.org/' \
+mysqldump $OPTS -h127.0.0.1 -P3306 -ucraggy | sed -e '/^-- Server version/d' \
+						  -e 's/^-- Host: 127.0.0.1/-- Host: flatcap.org/' \
+						  -e 's/\(DEFINER=`\)root/\1craggy/' \
 							> craggy_flatcap.sql
 
 

9479fc1b04ce05a2d65202fba712a4f553ef6609
Richard Russon (flatcap) <richard.russon@gmail.com>
2011-01-02 23:38:30 +0000

    create/drop shadow tables

---
+++ scripts/0031_drop_shadow_tables.sql
@@ -0,0 +1,14 @@
+drop view if exists climb;
+drop view if exists climb_note;
+drop view if exists climb_type;
+drop view if exists climber;
+drop view if exists colour;
+drop view if exists data;
+drop view if exists difficulty;
+drop view if exists grade;
+drop view if exists panel;
+drop view if exists rating;
+drop view if exists route;
+drop view if exists route_note;
+drop view if exists setter;
+drop view if exists success;

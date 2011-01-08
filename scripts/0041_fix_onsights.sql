d3ead9c104b27f1a14c7a2365cc7a31b653684f8
Richard Russon (flatcap) <richard.russon@gmail.com>
2011-01-08 02:21:48 +0000

    fix onsights

---
+++ scripts/0041_fix_onsights.sql
@@ -0,0 +1,11 @@
+# fix a few onsights
+
+update craggy_rating set onsight=0 where id=295;
+update craggy_rating set onsight=0 where id=297;
+update craggy_rating set onsight=0 where id=301;
+update craggy_rating set onsight=0 where id=112;
+update craggy_rating set onsight=0 where id=118;
+update craggy_rating set onsight=1 where id=263;
+update craggy_rating set onsight=0 where id=308;
+update craggy_rating set onsight=0 where id=313;
+update craggy_rating set onsight=0 where id=222;

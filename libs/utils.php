e3cbf53902594367d48eb6c307329fe04dded40c
Richard Russon (flatcap) <richard.russon@gmail.com>
2011-01-10 00:51:28 +0000

    remove dark style

--- libs/utils.php
+++ libs/utils.php
@@ -8,7 +8,6 @@ function html_header ($title, $reldir = '')
 	$output .= '<html>';
 	$output .= '<head>';
 	$output .= "<link rel='stylesheet' title='Purple' href='{$reldir}style/style.css' type='text/css'>";
-	$output .= "<link rel='alternate stylesheet' title='Dark' href='{$reldir}style/style_dark.css' type='text/css'>";
 	$output .= "<link rel='alternate' title='Craggy RSS' href='http://craggy.russon.org/rss.xml' type='application/rss+xml'>";
 	$output .= "<meta http-equiv='Content-Type' content='text/html; charset=utf-8'>";
 	$output .= "<title>$title - Craggy Island</title>";

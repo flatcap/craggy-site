f69bbae03c159669626dfdf7347e9ac22a3c67de
Richard Russon (flatcap) <richard.russon@gmail.com>
2011-01-11 14:10:01 +0000

    show the triggers

--- database_compare.sh
+++ database_compare.sh
@@ -7,21 +7,20 @@
 OPTS="--skip-extended-insert --add-drop-database --skip-dump-date --single-transaction"
 
 DATABASE="craggy"
-TABLES=(climb climb_note climb_type climber colour data difficulty grade panel \
-	rating route route_note setter success v_panel v_route)
+TABLES=(climb climb_note climb_type climber colour data difficulty grade panel rating route route_note setter success v_route)
 
 if [ ! -f craggy_russon.sql ]; then
 	mysqldump $OPTS -h127.0.0.1 -P3307 -ubackup -pphokio10 $DATABASE ${TABLES[*]} \
 		| sed	-e '/^-- Server version/d' \
 			-e 's/^-- Host: 127.0.0.1/-- Host: russon.org/' \
-			-e '/DEFINER=/d' \
+			-e 's/\(DEFINER=\)`root`/\1`craggy`/' \
 		> craggy_russon.sql
 fi
 
 mysqldump $OPTS -h127.0.0.1 -P3306 -ucraggy $DATABASE ${TABLES[*]} \
 	| sed	-e '/^-- Server version/d' \
 		-e 's/^-- Host: 127.0.0.1/-- Host: flatcap.org/' \
-		-e '/DEFINER=/d' \
+		-e 's/\(DEFINER=\)`root`/\1`craggy`/' \
 	> craggy_flatcap.sql
 
 dwdiff -c -C 3 --color=red,yellow craggy_russon.sql craggy_flatcap.sql | less -E

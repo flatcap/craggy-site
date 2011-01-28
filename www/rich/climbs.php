112f419e83d5ddf4592e915fef9f762707ab71bf
Richard Russon (flatcap) <richard.russon@gmail.com>
2011-01-28 17:41:22 +0000

    climbs is working again

--- www/rich/climbs.php
+++ www/rich/climbs.php
@@ -25,31 +25,33 @@ function climbs_main($options)
 	$climber_id = 1;
 
 	$table   = $DB_ROUTE .
-			" left join $DB_CLIMB on (($DB_CLIMB.route_id = $DB_ROUTE.id) and (climber_id = {$climber_id}))" .
-			" left join $DB_COLOUR on ($DB_ROUTE.colour_id = $DB_COLOUR.id)" .
-			" left join $DB_PANEL on ($DB_ROUTE.panel_id = $DB_PANEL.id)" .
-			" left join $DB_GRADE on ($DB_ROUTE.grade_id = $DB_GRADE.id)" .
-			" left join $DB_CLIMB_TYPE on ($DB_PANEL.climb_type_id = $DB_CLIMB_TYPE.id)" .
-			" left join $DB_SUCCESS on ($DB_CLIMB.success_id = $DB_SUCCESS.id)" .
-			" left join $DB_DIFFICULTY on ($DB_CLIMB.difficulty_id = $DB_DIFFICULTY.id)";
+			" left join $DB_CLIMB      on (($DB_CLIMB.route_id      = $DB_ROUTE.id) and (climber_id = {$climber_id}))" .
+			" left join $DB_COLOUR     on ($DB_ROUTE.colour_id      = $DB_COLOUR.id)" .
+			" left join $DB_PANEL      on ($DB_ROUTE.panel_id       = $DB_PANEL.id)" .
+			" left join $DB_GRADE      on ($DB_ROUTE.grade_id       = $DB_GRADE.id)" .
+			" left join $DB_CLIMB_TYPE on ($DB_PANEL.climb_type_id  = $DB_CLIMB_TYPE.id)" .
+			" left join $DB_SUCCESS    on ($DB_CLIMB.success_id     = $DB_SUCCESS.id)" .
+			" left join $DB_RATING     on ($DB_RATING.route_id      = $DB_ROUTE.id)" .
+			" left join $DB_DIFFICULTY on ($DB_RATING.difficulty_id = $DB_DIFFICULTY.id)" .
+			" left join $DB_CLIMB_NOTE on ($DB_RATING.climb_note_id = $DB_CLIMB_NOTE.id)";
+
 
 	$columns = array ("$DB_ROUTE.id as id",
-			"$DB_PANEL.name as panel",
-			"$DB_PANEL.sequence as panel_seq",
-			"$DB_COLOUR.colour as colour",
-			"$DB_GRADE.grade as grade",
-			"$DB_GRADE.sequence as grade_seq",
-			'climber_id',
-			'date_climbed',
-			'climb_type',
-			"$DB_SUCCESS.outcome as success",
-			'nice as n',
-			'onsight as o',
-			"$DB_DIFFICULTY.description as diff",
-			"$DB_CLIMB.notes as notes");
+			  "$DB_PANEL.name             as panel",
+		//	  "$DB_PANEL.sequence         as panel_seq",
+			  "$DB_COLOUR.colour          as colour",
+			  "$DB_GRADE.grade            as grade",
+		//	  "$DB_GRADE.sequence         as grade_seq",
+			  'climb_type',
+			  'date_climbed',
+			  "$DB_SUCCESS.outcome        as success",
+			  "nice                       as n",
+			  "onsight                    as o",
+			  "$DB_DIFFICULTY.description as diff",
+			  "notes");
 
-	$where   = null;
-	$order   = 'panel_seq, grade_seq, colour';
+	$where   = array ('date_end is null');
+	$order   = "$DB_PANEL.sequence, $DB_GRADE.sequence, colour, date_climbed";
 
 	$list = db_select($table, $columns, $where, $order);
 	$count = count($list);
@@ -74,18 +76,23 @@ function climbs_main($options)
 
 			$output .= html_header ('Climbs', '../');
 			$output .= '<body>';
+			$output .= "<div id='header'>";
+			$output .= "<img alt='craggy logo' width='135' height='66' src='../img/craggy.png'>";
+			$output .= "</div>";
 
-			$output .= "<div class='download'>";
-			$output .= '<h1>Route Data</h1>';
-			$output .= "<a href='?format=text'><img alt='climb list as a formatted text document' width='24' height='24' src='../img/txt.png'></a>";
-			$output .= '&nbsp;&nbsp;';
-			$output .= "<a href='?format=csv'><img alt='climb list as a csv document' width='24' height='24' src='../img/ss.png'></a>";
+			$output .= html_menu('../');
+
+			$output .= "<div id='title'>";
+			$output .= "<h1>Climbs</h1> <span>(Last updated: $last_update)</span></div>\n";
 			$output .= '</div>';
 
-			$output .= "<div class='header'>Climbs <span>(Last updated: $last_update)</span></div>\n";
-			$output .= html_menu('../');
-			$output .= "<div class='content'>";
-			$output .= "<h2>Climbs <span>({$count})</span></h2>";
+			$output .= "<div id='download'>";
+			$output .= '<h3>Route Data</h3>';
+			$output .= "<a href='?format=text'><img alt='climb list as a formatted text document' width='32' height='32' src='../img/txt.png'></a>";
+			$output .= "<a href='?format=csv'><img alt='climb list as a csv document' width='32' height='32' src='../img/ss.png'></a>";
+			$output .= '</div>';
+
+			$output .= "<div id='content'>";
 			$output .= list_render_html ($list, $columns, $widths, '{sortlist: [[0,0], [2,0], [1,0]]}');
 			$output .= '</div>';
 			$output .= get_errors();

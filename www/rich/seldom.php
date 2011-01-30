4e938bf48af6d718e092c65e9afcad2847b64e2d
Richard Russon (flatcap) <richard.russon@gmail.com>
2011-01-30 01:24:14 +0000

    fixed up seldom

--- www/rich/seldom.php
+++ www/rich/seldom.php
@@ -5,6 +5,40 @@ set_include_path ('../../libs');
 include 'db.php';
 include 'utils.php';
 
+function process_best ($list)
+{
+	$result = array();
+
+	$id = 0;
+	foreach ($list as $row) {
+		if ($row['route_id'] == $id) {
+			// duplicate
+			$d1 = $result[$id]['date_climbed'];
+			$d2 = $row['date_climbed'];
+			if ($d2 > $d1) {
+				$result[$id]['date_climbed'] = $d2;
+			}
+
+			$s1 = intval ($result[$id]['success_id']);
+			$s2 = intval ($row['success_id']);
+			if ($s2 > $s1) {
+				$result[$id]['success_id'] = $s2;
+				$result[$id]['success'] = $row['success'];
+			}
+
+			if ($row['notes'])
+				$results[$id]['notes'] = $row['notes'];
+		} else {
+			// copy the whole row
+			$id = $row['route_id'];
+			$result[$id] = $row;
+			$result[$id]['o'] = ($row['success_id'] > 2);	// Generate the onsight column
+		}
+	}
+
+	return $result;
+}
+
 function seldom_range ($m_start, $m_finish, $options)
 {
 	include 'db_names.php';
@@ -16,31 +50,34 @@ function seldom_range ($m_start, $m_finish, $options)
 	$climber_id = 1;
 
 	$table   = $DB_ROUTE .
-			" left join $DB_CLIMB on (($DB_CLIMB.route_id = $DB_ROUTE.id) and (climber_id = {$climber_id}))" .
-			" left join $DB_COLOUR on ($DB_ROUTE.colour_id = $DB_COLOUR.id)" .
-			" left join $DB_PANEL on ($DB_ROUTE.panel_id = $DB_PANEL.id)" .
-			" left join $DB_GRADE on ($DB_ROUTE.grade_id = $DB_GRADE.id)" .
-			" left join $DB_CLIMB_TYPE on ($DB_PANEL.climb_type_id = $DB_CLIMB_TYPE.id)" .
-			" left join $DB_SUCCESS on ($DB_CLIMB.success_id = $DB_SUCCESS.id)" .
-			" left join $DB_DIFFICULTY on ($DB_CLIMB.difficulty_id = $DB_DIFFICULTY.id)";
+			" left join $DB_CLIMB      on (($DB_CLIMB.route_id      = $DB_ROUTE.id) and (climber_id = {$climber_id}))" .
+			" left join $DB_COLOUR     on ($DB_ROUTE.colour_id      = $DB_COLOUR.id)" .
+			" left join $DB_PANEL      on ($DB_ROUTE.panel_id       = $DB_PANEL.id)" .
+			" left join $DB_GRADE      on ($DB_ROUTE.grade_id       = $DB_GRADE.id)" .
+			" left join $DB_CLIMB_TYPE on ($DB_PANEL.climb_type_id  = $DB_CLIMB_TYPE.id)" .
+			" left join $DB_SUCCESS    on ($DB_CLIMB.success_id     = $DB_SUCCESS.id)" .
+			" left join $DB_RATING     on ($DB_RATING.route_id      = $DB_ROUTE.id)" .
+			" left join $DB_DIFFICULTY on ($DB_RATING.difficulty_id = $DB_DIFFICULTY.id)" .
+			" left join $DB_CLIMB_NOTE on ($DB_RATING.climb_note_id = $DB_CLIMB_NOTE.id)";
 
-	$columns = array ("$DB_ROUTE.id as id",
-			"$DB_PANEL.name as panel",
-			"$DB_PANEL.sequence as panel_seq",
-			"$DB_COLOUR.colour as colour",
-			"$DB_GRADE.grade as grade",
-			"$DB_GRADE.sequence as grade_seq",
-			'climber_id',
-			'date_climbed',
-			'climb_type',
-			"$DB_SUCCESS.outcome as success",
-			'nice as n',
-			'onsight as o',
-			"$DB_DIFFICULTY.description as diff",
-			"$DB_CLIMB.notes as notes");
+	$columns = array (
+			  "$DB_ROUTE.id               as route_id",
+		//	  "$DB_CLIMB.id               as climb_id",
+			  "$DB_PANEL.name             as panel",
+		//	  "$DB_PANEL.sequence         as panel_seq",
+			  "$DB_COLOUR.colour          as colour",
+			  "$DB_GRADE.grade            as grade",
+		//	  "$DB_GRADE.sequence         as grade_seq",
+			  "climb_type",
+			  "date_climbed",
+			  "success_id",
+			  "$DB_SUCCESS.outcome        as success",
+			  "nice                       as n",
+			  "$DB_DIFFICULTY.description as diff",
+			  "notes");
 
-	$where   = array ("$DB_GRADE.sequence < 600");
-	$order = 'panel_seq, grade_seq, colour';
+	$where   = array ('date_end is null', "$DB_GRADE.sequence < 600");
+	$order   = "$DB_PANEL.sequence, $DB_GRADE.sequence, colour, date_climbed";
 
 	if (isset ($m_finish)) {
 		array_push ($where, "date_climbed < '$when_start'");
@@ -51,7 +88,9 @@ function seldom_range ($m_start, $m_finish, $options)
 	}
 
 	// print data (based on column names)
-	$list = db_select($table, $columns, $where, $order);
+	$list = db_select2($table, $columns, $where, $order);
+
+	$list = process_best ($list);
 
 	$today = strtotime('today');
 	// manipulate data (Lead -> L)
@@ -93,17 +132,23 @@ function seldom_main ($options)
 
 			$output .= html_header ('Seldom', '../');
 			$output .= '<body>';
+			$output .= "<div id='header'>";
+			$output .= "<img alt='craggy logo' width='135' height='66' src='../img/craggy.png'>";
+			$output .= "</div>";
 
-			$output .= "<div class='download'>";
-			$output .= '<h1>Route Data</h1>';
-			$output .= "<a href='?format=text'><img alt='seldom list as a formatted text document' width='24' height='24' src='../img/txt.png'></a>";
-			$output .= '&nbsp;&nbsp;';
-			$output .= "<a href='?format=csv'><img alt='seldom list as a csv document' width='24' height='24' src='../img/ss.png'></a>";
+			$output .= html_menu('../');
+
+			$output .= "<div id='title'>";
+			$output .= "<h1>Seldom</h1> <span>(Last updated: $last_update)</span></div>";
 			$output .= '</div>';
 
-			$output .= "<div class='header'>Seldom <span>(Last updated: $last_update)</span></div>";
-			$output .= html_menu('../');
-			$output .= "<div class='content'>";
+			$output .= "<div id='download'>";
+			$output .= '<h3>Route Data</h3>';
+			$output .= "<a href='?format=text'><img alt='seldom list as a formatted text document' width='32' height='32' src='../img/txt.png'></a>";
+			$output .= "<a href='?format=csv'><img alt='seldom list as a csv document' width='32' height='32' src='../img/ss.png'></a>";
+			$output .= '</div>';
+
+			$output .= "<div id='content'>";
 			break;
 
 		case 'csv':

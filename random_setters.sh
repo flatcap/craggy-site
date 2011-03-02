9f0d67f6677afeaf2f286b8137213256823d5556
Richard Russon (flatcap) <richard.russon@gmail.com>
2011-03-02 23:47:57 +0000

    db updates

--- random_setters.sh
+++ random_setters.sh
@@ -6,21 +6,21 @@ SURNAMES="$HOME/git/tips/surnames.txt"
 
 # clear tables
 echo
-echo "truncate craggy_climb;"
-echo "truncate craggy_climber;"
-echo "truncate craggy_route;"
-echo "truncate craggy_setter;"
+echo "truncate climb;"
+echo "truncate climber;"
+echo "truncate route;"
+echo "truncate setter;"
 echo
 
 # 10 random girl setters
-echo "insert into craggy_setter (initials, first_name, surname) values "
+echo "insert into setter (initials, first_name, surname) values "
 paste	<(sort --random-sort $GIRLS)											\
 	<(sort --random-sort $SURNAMES) | head -n 10 |								\
 	awk '{ printf "\t(\"%s%s\", \"%s\", \"%s\"),\n", tolower(substr($1,1,1)), tolower(substr($2,1,1)), $1, $2 }' |	\
 	sed '$s/,$/;\n/'
 
 # 100 random boy climbers
-echo "insert into craggy_climber (first_name, surname) values"
+echo "insert into climber (first_name, surname) values"
 paste	<(sort --random-sort $BOYS)											\
 	<(sort --random-sort $SURNAMES) | head -n 100 |								\
 	awk '{ printf "\t(\"%s\", \"%s\"),\n", $1, $2 }' |								\
@@ -32,7 +32,7 @@ paste	<(sort --random-sort $BOYS)											\
 #  20 grades
 #  10 setters
 # 200 max days old
-echo "insert into craggy_route (panel_id, colour_id, grade_id, setter_id, date_set) values"
+echo "insert into route (panel_id, colour_id, grade_id, setter_id, date_set) values"
 for ((i = 0; i < 200; i++)); do
 	PANEL=$((RANDOM%86+1))
 	COLOUR=$((RANDOM%21+1))
@@ -55,7 +55,7 @@ done | sed '$s/,$/;\n/'
 # 200 route_id
 # 200 days ago date_climbed
 # 4 success_id
-echo "insert into craggy_climb (climber_id, route_id, success_id, date_climbed) values"
+echo "insert into climb (climber_id, route_id, success_id, date_climbed) values"
 for ((i = 0; i < 5000; i++)); do
 	CLIMBER=$((RANDOM%100+1))
 	ROUTE=$((RANDOM%200+1))

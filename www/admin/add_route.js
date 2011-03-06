bb066e925c9331f884b905d44bbc8d26d1b8ae0a
Richard Russon (flatcap) <richard.russon@gmail.com>
2011-03-06 01:55:41 +0000

    typo

--- www/admin/add_route.js
+++ www/admin/add_route.js
@@ -27,7 +27,7 @@ function initialise_buttons()
 	entry_panel.focus();
 
 	entry_date   = document.getElementById ('date');
-	input_initialise ('date', 'grade');
+	input_initialise ('date', 'date');
 	entry_date.focus(); //tmp
 
 	entry_setter = document.getElementById ('setter');

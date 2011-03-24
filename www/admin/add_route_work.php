5a8173448877ab0a8cbd4a84bf2c172274e185db
Richard Russon (flatcap) <richard.russon@gmail.com>
2011-03-24 02:00:28 +0000

    much tidying and simplification

--- www/admin/add_route_work.php
+++ www/admin/add_route_work.php
@@ -7,193 +7,52 @@ set_include_path ('../../libs');
 include 'utils.php';
 include 'db.php';
 include 'db_names.php';
-include 'cache.php';
+
 include 'colour.php';
 include 'date.php';
+include 'grade.php';
+include 'panel.php';
 include 'setter.php';
-
-function parse_grade ($text)
-{
-	global $DB_GRADE;
-	static $grades = null;
-
-	if (!$grades)
-		$grades = cache_get_table ($DB_GRADE);
-
-	foreach ($grades as $g) {
-		if ($text == $g['grade'])
-			return $g;
-	}
-
-	return null;
-}
-
-function parse_panel ($text)
-{
-	global $DB_PANEL;
-	static $panels = null;
-
-	if (!$panels)
-		$panels = cache_get_table ($DB_PANEL);
-
-	foreach ($panels as $p) {
-		if ($text == $p['name'])
-			return $p;
-	}
-
-	return null;
-}
-
-function parse_setter ($text)
-{
-	// Need to match:
-	//     initials
-	//     unique first_name
-	//     unique surname
-	//     first_name surname
-
-	global $DB_SETTER;
-	static $setters = null;
-
-	if (empty ($text))
-		return null;
-	if (!$setters)
-		$setters = cache_get_table ($DB_SETTER);
-
-	$count = 0;
-	$match = null;
-	$text = strtolower ($text);
-	foreach ($setters as $s) {
-		$inits   = strtolower ($s['initials']);
-		$first   = strtolower ($s['first_name']);
-		$surname = strtolower ($s['surname']);
-		$whole   = "$first $surname";
-		if ($text == $whole)
-			return $s;
-		if ($text == $inits)
-			return $s;
-		if ($text == $first) {
-			$count++;
-			$match = $s;
-		}
-		if ($text == $surname) {
-			$count++;
-			$match = $s;
-		}
-	}
-
-	// check there's only one match
-	if ($count == 1)
-		return $match;
-
-	return null;
-}
-
-
-function valid_colour (&$route)
-{
-	$c = colour_match (urldecode ($route->colour));
-	if ($c !== null) {
-		$route->colour = $c['colour'];
-		$route->colour_id = $c['id'];
-		return true;
-	} else {
-		$route->addChild ('message', sprintf ("'%s' is not a valid colour", $route->colour));
-		return false;
-	}
-}
-
-function valid_date (&$route)
-{
-	$d = strtotime (urldecode ($route->date));
-
-	if ($d !== false) {
-		$now = strtotime ('now');
-		if ($d <= $now) {
-			$route->date = strftime('%Y-%m-%d', $d);
-			return true;
-		}
-		$route->addChild ('message', 'Date cannot be in the future');
-	} else {
-		$route->addChild ('message', sprintf ("'%s' is not a valid date", $route->date));
-	}
-
-	return false;
-}
-
-function valid_grade (&$route)
-{
-	$g = parse_grade (urldecode ($route->grade));
-	if ($g !== null) {
-		$route->grade    = $g['grade'];
-		$route->grade_id = $g['id'];
-		return true;
-	} else {
-		$route->addChild ('message', sprintf ("'%s' is not a valid grade", $route->grade));
-		return false;
-	}
-}
-
-function valid_notes (&$route)
-{
-	// check for invalid characters
-	$route->notes = urldecode ($route->notes);
-	return true;
-}
-
-function valid_panel (&$route)
-{
-	$p = parse_panel (urldecode ($route->panel));
-	if ($p !== null) {
-		$route->panel    = $p['name'];
-		$route->panel_id = $p['id'];
-		return true;
-	} else {
-		$route->addChild ('message', sprintf ("'%s' is not a valid panel", $route->panel));
-		return false;
-	}
-}
-
-function valid_setter (&$route)
-{
-	$s = parse_setter (urldecode ($route->setter));
-	if ($s !== null) {
-		$route->setter    = $s['first_name'] . ' ' . $s['surname'];
-		$route->setter_id = $s['id'];
-		return true;
-	} else {
-		$route->addChild ('message', sprintf ("'%s' is not a valid setter", $route->setter));
-		return false;
-	}
-}
-
 
 function validate_route (&$route)
 {
-	$colour = valid_colour ($route);
-	$date   = valid_date   ($route);
-	$grade  = valid_grade  ($route);
-	$notes  = valid_notes  ($route);
-	$panel  = valid_panel  ($route);
-	$setter = valid_setter ($route);
+	$colour = colour_match_xml ($route, urldecode ($route->colour));
+	if ($colour) {
+		$route->colour    = $c['colour'];
+		$route->colour_id = $c['id'];
+	}
 
-	return ($colour && $date && $grade && $notes && $panel && $setter);
+	$date = date_match_xml ($route, urldecode ($route->date));
+
+	$grade = grade_match_xml ($route, urldecode ($route->grade));
+	if ($grade) {
+		$route->grade    = $g['grade'];
+		$route->grade_id = $g['id'];
+	}
+
+	// XXX check for invalid characters?
+	$route->notes = urldecode ($route->notes);
+
+	$panel = panel_match_xml ($route, urldecode ($route->panel));
+	if ($panel) {
+		$route->panel    = $p['name'];
+		$route->panel_id = $p['id'];
+	}
+
+	$setter = setter_match_xml ($route, urldecode ($route->setter));
+	if ($setter) {
+		$route->setter    = $s['name'];
+		$route->setter_id = $s['id'];
+	}
+
+	return ($colour && $date && $grade && $panel && $setter);
 }
 
 function db_route_add ($route)
 {
-	/*
-	printf ("panel   = %s (%d)\n", $route->panel,  $route->panel_id);
-	printf ("colour  = %s (%d)\n", $route->colour, $route->colour_id);
-	printf ("grade   = %s (%d)\n", $route->grade,  $route->grade_id);
-	printf ("setter  = %s (%d)\n", $route->setter, $route->setter_id);
-	printf ("date    = %s\n",      $route->date);
-	printf ("notes   = %s\n",      $route->notes);
-	*/
-
-	$query = "insert into route (panel_id,colour_id,grade_id,setter_id,notes,date_set) values ";
+	global $DB_ROUTE
+	$query = "insert into $DB_ROUTE (panel_id,colour_id,grade_id,setter_id,notes,date_set) values ";
 	$query .= "($route->panel_id, $route->colour_id, $route->grade_id, $route->setter_id, '$route->notes', '$route->date')";
-	//echo $query . "\n";
 
 	$db = db_get_database();
 	$result = mysql_query($query);
@@ -341,18 +200,5 @@ function route_main()
 }
 
 
-/*
-//$_GET = array('action' => 'add', 'data' => '45   red  5+,     gn   6a  ,  bg   6b  ');
-if (isset ($argc)) {
-} else {
-	header('Content-Type: application/xml; charset=ISO-8859-1');
-	//echo "<pre>";
-}
-
-$xml = file_get_contents ('route.xml');
-$_GET = array('action' => 'save', 'data' => $xml);
-*/
-$result = route_main();
-//$result = htmlentities ($result);
-echo $result;
+echo route_main();
 

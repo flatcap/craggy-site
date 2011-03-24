0d55dd501d81598c3792c0665016737e57f285b1
Richard Russon (flatcap) <richard.russon@gmail.com>
2011-03-24 12:56:25 +0000

    write directly to log

--- libs/log.php
+++ libs/log.php
@@ -1,23 +1,30 @@
 <?php
 
+static $fd = null;
+
 function log_init ($path = null)
 {
+	global $fd;
+
 	if ($path)
 		ini_set('error_log', $path);
 	ini_set('display_errors', 0);
 	ini_set('log_errors', 1);
 	libxml_use_internal_errors (true);
-	log_string (strftime ('Log started on %Y-%m-%d as %H:%M', strtotime('now')));
+
+	$fd = fopen ($path, "a");
 }
 
 function log_var ($var)
 {
-	trigger_error (sprintf ("%s", print_r ($var, true)));
+	global $fd;
+	fwrite ($fd, print_r ($var, true));
 }
 
 function log_string ($str)
 {
-	trigger_error ($str);
+	global $fd;
+	fwrite ($fd, $str);
 }
 
 

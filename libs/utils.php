642892ecd641d811a8f9068109988d29d68ad460
Richard Russon (flatcap) <richard.russon@gmail.com>
2011-09-28 11:25:34 +0100

    moved to russon.org/craggy

--- libs/utils.php
+++ libs/utils.php
@@ -8,7 +8,7 @@ function html_header ($title, $reldir = '')
 	$output .= '<html>';
 	$output .= '<head>';
 	$output .= "<link rel='stylesheet' title='green' href='{$reldir}style/style.css' type='text/css'>";
-	$output .= "<link rel='alternate' title='Craggy RSS' href='http://craggy.russon.org/rss.xml' type='application/rss+xml'>";
+	$output .= "<link rel='alternate' title='Craggy RSS' href='http://russon.org/craggy/rss.xml' type='application/rss+xml'>";
 	$output .= "<meta http-equiv='Content-Type' content='text/html; charset=utf-8'>";
 	$output .= "<title>$title - Craggy Island</title>";
 
--- old/6a_prep.sh
+++ old/6a_prep.sh
@@ -3,7 +3,7 @@
 oocalc guildford_6a.ods &
 
 TMP="/tmp/info.$RANDOM"
-wget -O $TMP "http://craggy.russon.org/6a.php?format=csv"
+wget -O $TMP "http://russon.org/craggy/6a.php?format=csv"
 COUNT=$(wc -l $TMP | cut -d\  -f 1)
 COUNT=$((COUNT-1))
 
